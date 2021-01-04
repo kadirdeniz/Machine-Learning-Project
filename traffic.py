c0bcb7a6f0c2018dc6943cfdf83b85dc3c3add54
Kadir Deniz <deniz-kadir2014@hotmail.com>
2021-01-04 23:04:17 +0300

    Update traffic.py

--- traffic.py
+++ traffic.py
@@ -6,12 +6,9 @@ import tensorflow as tf
 
 from sklearn.model_selection import train_test_split
 
-EPOCHS = 10
 IMG_WIDTH = 30
 IMG_HEIGHT = 30
-NUM_CATEGORIES = 43
 TEST_SIZE = 0.4
-
 
 def main():
 
@@ -27,7 +24,6 @@ def main():
     x_train, x_test, y_train, y_test = train_test_split(
         np.array(images), np.array(labels), test_size=TEST_SIZE
     )
-
 
 
 def load_data(data_dir):
